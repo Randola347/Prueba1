6ab468d9e42fd7dd3d89a72da3355c377839d858
Randola347 <lpperezo84@gmail.com>
2023-10-04 09:44:35 -0600

    Creacion del repositorio e interfaz grafica

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+import GUI.GUI;
+
+/**
+ *
+ * @author lpper
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI gui = new GUI();
+        gui.setVisible(true);
+    }
+    
+}
